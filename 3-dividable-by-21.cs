a0bf698a0d1e64c5f76193a34ee0dccaf892746c
Said <saidig@code.edu.az>
2026-08-13 00:21:58 +0400

    add algo to check if x is div by both 3 and 5

---
+++ 3-dividable-by-21.cs
@@ -0,0 +1,10 @@
+int x = 42;
+
+if (x % 3 == 0 && x % 7 == 0)
+{
+    Console.WriteLine("x can be divided by both 3 and 7");
+}
+else
+{
+    Console.WriteLine("x does NOT satisfy the requirement");
+}
